9afd536c0dffee1f5ddb72f37e613553fce153e4
LENOVO <abdulfahad592@gmail.com>
2023-11-02 13:20:07 +0530

    Added new employee method

--- src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
+++ src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
@@ -4,4 +4,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+    public void employee(){
+        String firstName;
+        String lastName;
+        int empId;
+    }
+
 }
