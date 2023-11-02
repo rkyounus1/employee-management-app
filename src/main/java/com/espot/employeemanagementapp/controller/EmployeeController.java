e36c1934ae7ec1fa913d6c93edebf898b5e15181
Suhail-523 <mohammedsuhail95145@gmail.com>
2023-11-02 13:11:18 +0530

    Added getEmployee method in RestController

--- src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
+++ src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
@@ -1,7 +1,13 @@
 package com.espot.employeemanagementapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+@GetMapping
+public String getEmployee(){
+    String emp = "getEmployeeMethod";
+        return emp;
+    }
 }
