be9b0bbbdfe6d656e68d6b83c0f43cb16c15a4b5
Fahad-592 <abdulfahad592@gmail.com>
2023-11-02 14:48:31 +0530

    create employee method

--- src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
+++ src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
@@ -1,11 +1,21 @@
 package com.espot.employeemanagementapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-@GetMapping("/")
+    @PostMapping("/emp")
+    public <emp> String createEmp(@RequestBody emp a){
+         String empName="name";
+         return empName;
+
+
+    }
+
+    @GetMapping("/")
 public String getEmployee(){
     String emp = "EmployeeMethod ";
         return emp;
