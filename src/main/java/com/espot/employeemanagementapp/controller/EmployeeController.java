6e0526b8fc4d1bd98fe0452b3071436c50d69501
Suhail-523 <mohammedsuhail95145@gmail.com>
2023-11-04 23:03:51 +0530

    Added getEmployeeById method in RestController

--- src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
+++ src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import com.espot.employeemanagementapp.entity.Employee;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -18,15 +19,20 @@ public List<Employee> getEmployee(){
        List<Employee> list = employeeRepository.findAll();
         return list;
     }
+@GetMapping("/employee/{id}")
+public Optional<Employee> getEmployeeById(@PathVariable("id") Long employeeId){
+        Optional<Employee> empId = employeeRepository.findById(employeeId);
+        return empId;
+    }
 
-    @PostMapping("/")
-    public String createEmp(@RequestBody Employee employee){
+@PostMapping("/")
+public String createEmp(@RequestBody Employee employee){
     employeeRepository.save(employee);
     return "success";
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteEmployee(@PathVariable Long id) {
+@DeleteMapping("/{id}")
+public String deleteEmployee(@PathVariable Long id) {
             employeeRepository.deleteById(id);
             return "Employee with ID " + id + " has been deleted.";
 
