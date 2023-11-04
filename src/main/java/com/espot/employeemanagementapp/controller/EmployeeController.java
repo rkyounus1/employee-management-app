1395f14066a86f2438c59ff913739b8a710b874d
Suhail-523 <mohammedsuhail95145@gmail.com>
2023-11-04 13:13:35 +0530

    1.password changed in application.properties and changed database name. 2.Added getEmployee method and RequestMapping in RestController. 3.Changed Folder name into small letter

--- src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
+++ src/main/java/com/espot/employeemanagementapp/controller/EmployeeController.java
@@ -1,13 +1,23 @@
 package com.espot.employeemanagementapp.controller;
 
+import com.espot.employeemanagementapp.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.espot.employeemanagementapp.entity.Employee;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
+    @Autowired
+    EmployeeRepository employeeRepository;
 @GetMapping("/")
-public String getEmployee(){
-    String emp = "EmployeeMethod ";
-        return emp;
+public List<Employee> getEmployee(){
+       List<Employee> list = employeeRepository.findAll();
+        return list;
     }
+
 }
